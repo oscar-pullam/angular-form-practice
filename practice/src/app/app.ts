c696464c07fd60de93fb3e419af8e28a6d3785c1
Oscar Pullam <oscar.pullam@rolemodelsoftware.com>
2025-09-02 16:32:27 -0400

    Add mui

--- practice/src/app/app.ts
+++ practice/src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, MatFormFieldModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('practice');
+  name = new FormControl('');
 }
