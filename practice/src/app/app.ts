316fa62b72f32e80732627213074c03cae540c3f
Oscar Pullam <oscar.pullam@rolemodelsoftware.com>
2025-09-03 12:26:22 -0400

    Working on sidebar

--- practice/src/app/app.ts
+++ practice/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [MatInputModule, RouterOutlet, ReactiveFormsModule, MatFormFieldModule],
+  imports: [MatInputModule, RouterOutlet, ReactiveFormsModule, MatFormFieldModule, MatSidenavContent, MatSidenavContainer, MatSidenav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
