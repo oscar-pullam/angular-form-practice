77b4d47860b6f62c33d03cefe86240cef1c9c1f1
Oscar Pullam <oscar.pullam@rolemodelsoftware.com>
2025-09-03 11:53:48 -0400

    Fix input errror

--- practice/src/app/app.ts
+++ practice/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule, MatFormFieldModule],
+  imports: [MatInputModule, RouterOutlet, ReactiveFormsModule, MatFormFieldModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
